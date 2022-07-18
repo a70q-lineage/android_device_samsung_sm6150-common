3533d010ee523626d082db7e01374ad04474f9db
Simon1511 <simon2002.schoenmackers@gmail.com>
2022-07-18 15:57:37 +0200

    sm6150-common: Import camera hidl
    *From https://github.com/LineageOS/android_device_samsung_exynos9820-common/tree/lineage-19.1/hidl/camera/provider
    
    Change-Id: I0627668fba1c8ce0da67d161342871023155a159

--- common.mk
+++ common.mk
@@ -128,13 +128,13 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.6-service.sm6150 \
+    android.hardware.camera.provider@2.6.vendor \
+    android.hardware.camera.device@3.6.vendor \
     android.frameworks.cameraservice.common@2.0.vendor \
     android.frameworks.cameraservice.device@2.0.vendor \
     android.frameworks.cameraservice.service@2.1.vendor \
     android.frameworks.cameraservice.service@2.0.vendor \
-    android.hardware.camera.provider@2.6-service \
-    android.hardware.camera.provider@2.6.vendor \
-    android.hardware.camera.device@3.6.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Display/Graphics
