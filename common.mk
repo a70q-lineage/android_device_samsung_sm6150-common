491424f1b0f3ea6c117d413137a729d3378df33c
Simon1511 <simon2002.schoenmackers@gmail.com>
2022-11-26 22:28:48 +0100

    sm6150-common: Add UdfpsHandler for NightDisplay and ExtraDim
    *Optical fingerprint sensors don't like NightDisplay and ExtraDim since those decrease overall sensor accuracy
    *Turn ExtraDim off and set NightDisplay's color temperature to AOSP's default white balance value if the fingerprint sensor is touched
    
    Change-Id: I6651cd5c8c066102feebfb764184b099c6ad741d

--- common.mk
+++ common.mk
@@ -183,7 +183,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service-samsung.sm6150
+    android.hardware.biometrics.fingerprint@2.3-service-samsung.sm6150 \
+    SamsungUdfpsHandler.sm6150
 
 # FM
 # PRODUCT_PACKAGES += \
