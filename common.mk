ca74735a5ee596f17f266d2af85460e3df91c7ed
Andrzej Szafran <rtd1250@gmail.com>
2023-06-20 21:23:08 +0200

    sm6150-common: Switch to prebuilt Audio HAL, again The saga continues. Now that BT VOIP calls work fine on proprietary audio, we can have all the advantages of prebuilt while not sacrificing bluetooth calls.
    Fixes echoing in calls and very low mic/speaker volumes.
    
    This reverts:
    - https://github.com/rtd1250/android_device_samsung_a70q/commit/6cbf461855b704df0f4f2c99854ed0276cf0de1c
    - https://github.com/rtd1250/android_device_samsung_a70q/commit/a2322e40789b82e28f928f1b6ae74e3320b2499f
    - https://github.com/rtd1250/android_device_samsung_a70q/commit/d62e57d11524b1953c520a1c63203d3f3cdbca89
    - https://github.com/rtd1250/android_device_samsung_a70q/commit/5293f37e18228be968fb24cf96fa35fced3045ea
    
    Change-Id: I5a11f67a2630eb4f8b714c8ca77a4e48fbe865da

--- common.mk
+++ common.mk
@@ -83,7 +83,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl.sm6150 \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.2-impl \
-    audio.primary.sm6150 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
@@ -106,11 +105,14 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/audio/configs/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(COMMON_PATH)/audio/configs/graphite_ipc_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/graphite_ipc_platform_info.xml \
     $(COMMON_PATH)/audio/configs/listen_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/listen_platform_info.xml \
+    $(COMMON_PATH)/audio/configs/mixer_usb_default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_usb_default.xml \
+    $(COMMON_PATH)/audio/configs/mixer_usb_gray.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_usb_gray.xml \
     $(COMMON_PATH)/audio/configs/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(COMMON_PATH)/audio/configs/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
 PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
