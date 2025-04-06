a472980a4f9008769c52abe238dbe177e28ee3b9
Simon1511 <me@simon1511.de>
2025-04-06 12:58:33 +0200

    sm6150-common: rename sensors subhal
    Conflicts with common hardware/samsung one
    
    Change-Id: Id96a59b5e42e6416e5db520ce3aa223454ec979e

--- common.mk
+++ common.mk
@@ -401,7 +401,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.samsung-multihal \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
-    sensors.samsung \
+    sensors.samsung-sm6150 \
     android.frameworks.sensorservice@1.0.vendor
 
 # Sensor Configuration
